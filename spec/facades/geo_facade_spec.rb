0466b74135514521a572330bb7dfb23e2bdcfa1d
Sarah Hall <swatson1994@yahoo.com>
2022-08-06 18:12:54 -0400

    Remove unneeded mock

--- spec/facades/geo_facade_spec.rb
+++ spec/facades/geo_facade_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe GeoFacade, :vcr do
   it 'can make a location poro from a Geo call' do
-    example = File.read('./spec/fixtures/example_weather_call.json')
-    allow(OpenWeatherService).to receive(:get_forecast).and_return(JSON.parse(example, symbolize_names: true))
     location = GeoFacade.create_coordinates('Syracuse, NY')
 
     expect(location).to be_an_instance_of(Location)
