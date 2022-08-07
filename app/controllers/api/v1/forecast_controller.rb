06cd4b723ee766d9042de2b1b6754d7988a12b6b
Sarah Hall <swatson1994@yahoo.com>
2022-08-06 22:11:59 -0400

    Update controller, lead into correct serializer with correct information

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
 
   def index
-    render json: ForecastSerializer.format_forecast
+    loc = GeoFacade.create_coordinates(params[:location])
+    forecast = OpenWeatherFacade.create_forecast(loc.lat, loc.lon)
+    render json: ForecastSerializer.format_forecast(forecast)
   end
 end
