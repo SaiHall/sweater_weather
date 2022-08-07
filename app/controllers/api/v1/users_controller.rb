2ad63de86caa4f15b11b366a12fb191636a946f3
Sarah Hall <swatson1994@yahoo.com>
2022-08-07 19:52:31 -0400

    Move email re-write lower in controller to play nice with error handler

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = user_params
-    user[:email] = user[:email].downcase
     new_user = User.create!(user_params)
     if new_user.save
+      new_user[:email] = new_user[:email].downcase
       new_user[:api_key] = SecureRandom.hex if new_user[:api_key].nil?
       render json: UserSerializer.new(new_user), status: :created
     end
