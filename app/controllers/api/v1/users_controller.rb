0c1513c2f73c1d2391b9f1a07d79d916426913c7
Sarah Hall <swatson1994@yahoo.com>
2022-08-07 19:25:40 -0400

    Feat: Update controller and add serializer for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::UsersController < ApplicationController
-
   def create
+    user = user_params
+    user[:email] = user[:email].downcase
+    new_user = User.create!(user_params)
+    if new_user.save
+      new_user[:api_key] = SecureRandom.hex if new_user[:api_key].nil?
+      render json: UserSerializer.new(new_user), status: :created
+    else
+      render status: 400
+    end
+  end
 
+  private
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
