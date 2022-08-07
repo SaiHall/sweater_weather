21f1f0d350bf9001cf07e8b9b82a8fa35e439966
Sarah Hall <swatson1994@yahoo.com>
2022-08-07 19:52:08 -0400

    Test: Add tests for missing fields

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe 'User api call' do
     expect(user_return[:attributes][:api_key]).to be_a(String)
   end
 
-  describe 'Sad pat/ error handling' do
+  describe 'Sad path/ error handling' do
     it 'will not create a user if password and confirmation do not match' do
       user_params = {
                   email: 'Something@special.com',
@@ -68,6 +68,48 @@ RSpec.describe 'User api call' do
 
       response_body = JSON.parse(response.body, symbolize_names: true)
       expect(response_body[:message]).to eq("Validation failed: Email has already been taken")
+    end
+
+    it 'will not create a user if p/c field is missing' do
+      user_params = {
+                  email: 'Something@special.com',
+                  password: "1234"
+                }
+        headers = {"CONTENT_TYPE" => "application/json"}
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      expect(response_body[:message]).to eq("Validation failed: Password confirmation can't be blank")
+    end
+
+    it 'will not create a user if email field is missing' do
+      user_params = {
+                  password: "1234",
+                  password_confirmation: "1234"
+                }
+        headers = {"CONTENT_TYPE" => "application/json"}
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      expect(response_body[:message]).to eq("Validation failed: Email can't be blank")
+    end
+
+    it 'will not create a user if password field is missing' do #find out how to remove extra pw can't be blank. All in one message? Limit how many errors somehow?
+      user_params = {
+                  email: 'Something@special.com',
+                  password_confirmation: "1234"
+                }
+        headers = {"CONTENT_TYPE" => "application/json"}
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      expect(response_body[:message]).to eq("Validation failed: Password can't be blank, Password can't be blank")
     end
   end
 end
