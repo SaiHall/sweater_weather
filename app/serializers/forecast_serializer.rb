2b3833e38ab510158e975e9088e71d0ac3a30362
Sarah Hall <swatson1994@yahoo.com>
2022-08-06 22:11:13 -0400

    Update forecast serializer to include current, daily, and hourly

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,7 +1,44 @@
 class ForecastSerializer
 
-  def self.format_forecast
-    {}
+  def self.format_forecast(forecast)
+    {
+      data: {
+        id: nil,
+        type: "forecast",
+        attributes: {
+          current_weather: {
+            datetime: forecast.current.date_time,
+            sunrise: forecast.current.sunrise,
+            sunset: forecast.current.sunset,
+            temperature: forecast.current.temperature,
+            feels_like: forecast.current.feels_like,
+            humidity: forecast.current.humidity,
+            uvi: forecast.current.uvi,
+            visibility: forecast.current.visibility,
+            conditions: forecast.current.conditions,
+            icon: forecast.current.icon
+            },
+          daily_weather: forecast.daily.map do |day|
+            {
+              date: day.date_time,
+              sunrise: day.sunrise,
+              sunset: day.sunset,
+              max_temp: day.max_temp,
+              min_temp: day.min_temp,
+              conditions: day.conditions,
+              icon: day.icon
+            }
+          end,
+          hourly_weather: forecast.hourly.map do |hour|
+            {
+              time: hour.time,
+              temperature: hour.temperature,
+              conditions: hour.conditions,
+              icon: hour.icon
+            }
+          end,
+        }
+      }
+    }
   end
-
 end
