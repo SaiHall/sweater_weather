324546ce422a737afe09715c77e2431613a8236c
Sarah Hall <swatson1994@yahoo.com>
2022-08-06 21:01:20 -0400

    Test: Add inital test for internal api hit

---
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast API: City/State' do
+  it 'can successfully hit the api' do
+    get "/api/v1/forecast?location=denver, co"
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+  end
+end
