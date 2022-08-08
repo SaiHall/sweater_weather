deeed4cd76cc0f072a9da8de755b5927b7c14a13
Sarah Hall <swatson1994@yahoo.com>
2022-08-08 19:47:43 -0400

    Feat: Add create directions method for geo facade

--- app/facades/geo_facade.rb
+++ app/facades/geo_facade.rb
@@ -5,4 +5,9 @@ class GeoFacade
     refined_json = json[:results][0][:locations][0]
     Location.new(refined_json)
   end
+
+  def self.create_directions(origin, destination)
+    json = GeoService.get_directions(origin, destination)
+    RoadTrip.new(json[:route])
+  end
 end
