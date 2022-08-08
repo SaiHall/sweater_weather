5f931b60757fb6a941622e682252328819313bd5
Sarah Hall <swatson1994@yahoo.com>
2022-08-08 12:47:54 -0400

    Test: Further fill out munchie test to include data types and formatting

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -7,4 +7,31 @@ RSpec.describe 'Munchies API: location/term' do
     expect(response).to be_successful
     expect(response.status).to eq(200)
   end
+
+  it 'returns the desired information from a munchies api hit', :vcr do
+    get "/api/v1/munchies?location=denver,co&food=chinese"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    munch = response_body[:data]
+
+    expect(munch).to be_a(Hash)
+    expect(munch.keys).to include(:id, :type, :attributes)
+
+    expect(munch[:id]).to eq(nil)
+    expect(munch[:type]).to eq("munchie")
+    expect(munch[:attributes]).to be_a(Hash)
+
+    expect(munch[:attributes].keys).to include(:destination_city, :forecast, :restaurant)
+    expect(munch[:attributes][:destination_city]).to be_a(String)
+    expect(munch[:attributes][:forecast]).to be_a(Hash)
+    expect(munch[:attributes][:restaurant]).to be_a(Hash)
+
+    expect(munch[:attributes][:forecast].keys).to include(:summary, :temperature)
+    expect(munch[:attributes][:forecast][:summary]).to be_a(String)
+    expect(munch[:attributes][:forecast][:temperature]).to be_a(String)
+
+    expect(munch[:attributes][:restaurant].keys).to include(:name, :address)
+    expect(munch[:attributes][:restaurant][:name]).to be_a(String)
+    expect(munch[:attributes][:restaurant][:address]).to be_a(String)
+  end
 end
