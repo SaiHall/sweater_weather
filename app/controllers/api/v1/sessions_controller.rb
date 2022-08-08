71e11665ae8b0f01d787b0f97d0e4028610b9928
Sarah Hall <swatson1994@yahoo.com>
2022-08-08 15:32:43 -0400

    Feat: Require parameters in controller, and add missing params to EH

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-    user = User.find_by!(email: params[:email].downcase)
-    if user.authenticate(params[:password])
+    user = User.find_by!(email: session_params[:email].downcase)
+    if user.authenticate(session_params[:password])
       render json: UserSerializer.new(user)
     else
       json_response({message: "Invalid Credentials"}, 400)
     end
+  end
+
+  private
+
+  def session_params
+    params.require(:email)
+    params.require(:password)
+    params.permit(:email, :password)
   end
 end
