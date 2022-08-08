07a58712d413fbdf56cb439da791d12d5240b0da
Sarah Hall <swatson1994@yahoo.com>
2022-08-08 11:40:48 -0400

    Test: Add facade test for yelp service and business poro

---
+++ spec/facades/yelp_facade_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe YelpFacade, :vcr do
+  it 'can make a business poro from a Yelp call' do
+    business = YelpFacade.create_business('Syracuse, NY', 'Pizza')
+
+    expect(business).to be_an_instance_of(Bussiness)
+  end
+end
