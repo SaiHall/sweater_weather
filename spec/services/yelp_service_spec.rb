72d68a45a0ebe0aab115186a23c6eaaa994dd562
Sarah Hall <swatson1994@yahoo.com>
2022-08-08 11:32:43 -0400

    Test: Update test with expected result, use loc instead of coords

--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -2,6 +2,13 @@ require "rails_helper"
 
 RSpec.describe YelpService do
   it 'can return information from a call given a location and search term' do
-    returned_hash = YelpService.get_business('43.047945', '-76.147448', 'chinese')
+    returned_hash = YelpService.get_business('denver, co', 'chinese')
+
+    expect(returned_hash).to be_a(Hash)
+    expect(returned_hash).to have_key(:businesses)
+
+    expect(returned_hash[:businesses]).to be_an(Array)
+    expect(returned_hash[:businesses][0]).to be_a(Hash)
+    expect(returned_hash[:businesses][0].keys).to include(:name, :coordinates, :location)
   end
 end
