794162b45162b2d1b6d7104b80adf0293d86c71d
Sarah Hall <swatson1994@yahoo.com>
2022-08-08 11:57:19 -0400

    Test: Add fixture and fill in expected values

--- spec/poros/business_spec.rb
+++ spec/poros/business_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Business do
   it 'exists and has attributes' do
+    json = File.read('./spec/fixtures/business_data.json')
+    data = JSON.parse(json, symbolize_names: true)
 
-    business = Business.new(@data)
+    business = Business.new(data)
 
     expect(business).to be_an_instance_of(Business)
-    expect(business.name).to eq()
-    expect(business.address).to eq()
-    expect(business.lat).to eq()
-    expect(business.lon).to eq()
+    expect(business.name).to eq("Taste of China")
+    expect(business.address).to eq("1935 Federal Blvd Denver, CO 80204")
+    expect(business.lat).to eq(39.74702)
+    expect(business.lon).to eq(-105.02571)
   end
 end
