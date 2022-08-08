5eb6601b102f3f7a02a3293af54e163b6107b236
Sarah Hall <swatson1994@yahoo.com>
2022-08-08 12:55:34 -0400

    Feat: Update munchies controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::MunchiesController < ApplicationController
 
   def index
+    business = YelpFacade.create_business(params[:location], params[:food])
+    current = OpenWeatherFacade.create_current(business.lat, business.lon)
+    render json: MunchiesSerializer.format_munchies(business, current)
   end
-
 end
