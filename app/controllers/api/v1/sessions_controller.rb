a2a081aff4139016f5ffb9778bcef2a1b1002410
Sarah Hall <swatson1994@yahoo.com>
2022-08-07 20:53:00 -0400

    Feat: Adjust controller and exception handler for sp/ec tests

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,14 +1,9 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-    user = User.find_by(email: login_params[:email])
-    if user.authenticate(login_params[:password])
+    user = User.find_by!(email: params[:email].downcase)
+    if user.authenticate(params[:password])
       render json: UserSerializer.new(user)
     end
-  end
-
-
-  def login_params
-    params.permit(:email, :password)
   end
 end
