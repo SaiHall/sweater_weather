dc73dd7b0426ef70deaffd321f437a99d0bad990
Sarah Hall <swatson1994@yahoo.com>
2022-08-08 13:46:53 -0400

    Test: Add explicit testing for format methods in business poro

--- spec/poros/business_spec.rb
+++ spec/poros/business_spec.rb
@@ -1,17 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Business do
-  it 'exists and has attributes' do
+  before(:each) do
     json = File.read('./spec/fixtures/business_data.json')
     data = JSON.parse(json, symbolize_names: true)
 
-    business = Business.new(data)
+    @business = Business.new(data)
+  end
+  it 'exists and has attributes' do
 
-    expect(business).to be_an_instance_of(Business)
-    expect(business.name).to eq("Taste of China")
-    expect(business.address).to eq("1935 Federal Blvd Denver, CO 80204")
-    expect(business.city).to eq("Denver, CO")
-    expect(business.lat).to eq(39.74702)
-    expect(business.lon).to eq(-105.02571)
+
+    expect(@business).to be_an_instance_of(Business)
+    expect(@business.name).to eq("Taste of China")
+    expect(@business.address).to eq("1935 Federal Blvd Denver, CO 80204")
+    expect(@business.city).to eq("Denver, CO")
+    expect(@business.lat).to eq(39.74702)
+    expect(@business.lon).to eq(-105.02571)
+  end
+
+  it 'will format an address' do
+
+    expect(@business.address).to eq("1935 Federal Blvd Denver, CO 80204")
+    expect(@business.address).to_not eq(["1935 Federal Blvd", "Denver, CO 80204"])
+  end
+
+  it 'will format a city to include the state' do
+
+    expect(@business.city).to eq("Denver, CO")
+    expect(@business.address).to_not eq("Denver")
   end
 end
