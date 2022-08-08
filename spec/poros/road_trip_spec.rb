542a3a274fd95d4b0f16a3fe8f4a6c8147a6b881
Sarah Hall <swatson1994@yahoo.com>
2022-08-08 19:45:20 -0400

    Test: Create poro spec for road trip

---
+++ spec/poros/road_trip_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe RoadTrip do
+  it 'exists and has attributes' do
+    json = File.read('./spec/fixtures/example_directions_call.json')
+    data = JSON.parse(json, symbolize_names: true)
+
+    trip = RoadTrip.new(data[:route])
+
+    expect(trip).to be_an_instance_of(RoadTrip)
+    expect(trip.time).to eq("10:45:03")
+    expect(trip.start).to eq("North Port, FL")
+    expect(trip.end).to eq("New Orleans, LA")
+    expect(trip.lon).to eq(-90.079575)
+    expect(trip.lat).to eq(30.26137)
+  end
+end
