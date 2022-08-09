8e77adaefb1c00e0068f2cc9c5b0c0860c7fc337
Sarah Hall <swatson1994@yahoo.com>
2022-08-09 15:43:16 -0400

    Update controller with conditionals for impossible route

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,20 +1,20 @@
 class Api::V1::RoadTripController < ApplicationController
 
   def create
-    if !trip_params[:api_key] || !User.valid_key?(trip_params[:api_key])
-      json_response({message: "Unauthorized Request"}, 401)
+    json_response({message: "Unauthorized Request"}, 401) if !trip_params[:api_key] || !User.valid_key?(trip_params[:api_key])
+    trip = GeoFacade.create_directions(trip_params[:origin], trip_params[:destination])
+    if trip == "impossible route"
+      render json: RoadTripSerializer.format_impossible_route(trip_params[:origin], trip_params[:destination])
     else
-      trip = GeoFacade.create_directions(trip_params[:origin], trip_params[:destination])
       forecast = OpenWeatherFacade.create_future_weather(trip.lat, trip.lon, trip.hours)
       render json: RoadTripSerializer.format_road_trip(trip, forecast)
     end
   end
 
   private
-
   def trip_params
     params.require(:origin)
     params.require(:destination)
     params.permit(:origin, :destination, :api_key)
-    end
+  end
 end
