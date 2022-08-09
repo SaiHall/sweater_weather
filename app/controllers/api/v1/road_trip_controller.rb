3749436386e49d8961ddda4dddb548c63ca48a76
Sarah Hall <swatson1994@yahoo.com>
2022-08-08 20:23:46 -0400

    Save ending spot. Made params required, working with serializer next

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::RoadTripController < ApplicationController
 
   def create
+    # trip = GeoFacade.create_directions(trip_params[:origin], trip_params[:destination])
+    # forecast = OpenWeatherFacade.get_future_weather(trip.lat, lat.lon)
     render json: RoadTripSerializer.format_road_trip
   end
 
+  private
+
+  def trip_params
+    params.require(:origin)
+    params.require(:destination)
+    params.require(:api_key)
+    params.permit(:origin, :destination, :api_key)
+    end
 end
