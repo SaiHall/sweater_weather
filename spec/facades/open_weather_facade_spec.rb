423a218647ba66a24e0cc0e0be9579ce79e1a088
Sarah Hall <swatson1994@yahoo.com>
2022-08-09 13:42:37 -0400

    Test: Add tests for create future weather method

--- spec/facades/open_weather_facade_spec.rb
+++ spec/facades/open_weather_facade_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe OpenWeatherFacade, :vcr do
     expect(forecast.daily.length).to be(5)
     expect(forecast.hourly.length).to be(8)
   end
+
+  it 'will make an hourly poro for the furture, given a number of hours below 48' do
+    test = OpenWeatherFacade.create_future_weather('43.047945', '-76.147448', "10")
+    expect(test).to be_an_instance_of(Hourly)
+  end
+
+  it 'will make a daily poro given a number of hours greater than or equal to 48' do
+    test = OpenWeatherFacade.create_future_weather('43.047945', '-76.147448', "56")
+    expect(test).to be_an_instance_of(Daily)
+  end
 end
