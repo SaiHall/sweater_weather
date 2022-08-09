386c63195a752f5fb0aad3b8c6effa14e52ba46a
Sarah Hall <swatson1994@yahoo.com>
2022-08-09 13:49:47 -0400

    Test: Add test to include formatted hours in roadtrip poro

--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -13,5 +13,20 @@ RSpec.describe RoadTrip do
     expect(trip.end).to eq("New Orleans, LA")
     expect(trip.lon).to eq(-90.074951)
     expect(trip.lat).to eq(29.950621)
+    expect(trip.hours).to eq(10)
+  end
+
+  it 'will correctly format entered data' do
+    json = File.read('./spec/fixtures/example_directions_call.json')
+    data = JSON.parse(json, symbolize_names: true)
+
+    trip = RoadTrip.new(data[:route])
+
+    expect(trip.hours).to eq(10)
+    expect(trip.hours).to_not eq("10:45:03")
+
+    expect(trip.start).to eq("North Port, FL")
+    expect(trip.start).to_not eq("North Port")
+    expect(trip.start).to_not eq("FL")
   end
 end
