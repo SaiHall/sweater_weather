26ab8c529b9d9e898998f98bf6296461935b2b07
Sarah Hall <swatson1994@yahoo.com>
2022-08-09 14:16:38 -0400

    Test: Add missing parameter tests for destination and api key

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -73,5 +73,67 @@ RSpec.describe 'Road trip call', :vcr do
       response_body = JSON.parse(response.body, symbolize_names: true)
       expect(response_body[:message]).to eq("param is missing or the value is empty: origin")
     end
+
+    it 'will return an error if no end location is provided' do
+      bad_parameters = {
+                    origin: "Denver, CO",
+                    destination: "",
+                    api_key: "79012744298e72fbb257d621cbbde7ca"
+                    }
+
+      post "/api/v1/road_trip", headers: @headers, params: JSON.generate(bad_parameters)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      expect(response_body[:message]).to eq("param is missing or the value is empty: destination")
+    end
+
+    it 'will return an error if end location is missing' do
+      bad_parameters = {
+                    origin: "Denver, CO",
+                    api_key: "79012744298e72fbb257d621cbbde7ca"
+                    }
+
+      post "/api/v1/road_trip", headers: @headers, params: JSON.generate(bad_parameters)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      expect(response_body[:message]).to eq("param is missing or the value is empty: destination")
+    end
+
+    it 'will return an error if no api key is provided' do
+      bad_parameters = {
+                    origin: "Denver, CO",
+                    destination: "Pueblo, CO",
+                    api_key: ""
+                    }
+
+      post "/api/v1/road_trip", headers: @headers, params: JSON.generate(bad_parameters)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      expect(response_body[:message]).to eq("param is missing or the value is empty: api_key")
+    end
+
+    it 'will return an error if api key is missing' do
+      bad_parameters = {
+                    origin: "Denver, CO",
+                    destination: "Pueblo, CO"
+                    }
+
+      post "/api/v1/road_trip", headers: @headers, params: JSON.generate(bad_parameters)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      expect(response_body[:message]).to eq("param is missing or the value is empty: api_key")
+    end
   end
 end
