92deac487019de776559fb3d71f29aeff591ff14
Sarah Hall <swatson1994@yahoo.com>
2022-08-09 14:37:01 -0400

    Test and create class method to determine valid api key

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of(:password)}
     it {should validate_presence_of(:password_confirmation)}
   end
+
+  describe 'class methods' do
+    it 'will return true if an api key is valid, and false if it is not' do
+      User.create!(email: "Email@email.com", password: "12345", password_confirmation: "12345", api_key: SecureRandom.hex)
+      user = User.all.first
+
+      expect(User.valid_key?(user.api_key)).to eq(true)
+      expect(User.valid_key?("Pants")).to eq(false)
+    end
+  end
 end
