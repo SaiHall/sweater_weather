2a293cdebffcc7c182a4b79dcb4e540ee0cc8c19
Sarah Hall <swatson1994@yahoo.com>
2022-08-09 14:06:59 -0400

    Tst: Add test for response format and data types

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -17,4 +17,28 @@ RSpec.describe 'Road trip call', :vcr do
     expect(response).to be_successful
     expect(response.status).to eq(200)
   end
+
+  it 'returns the correctly formatted response from an api hit' do
+    post "/api/v1/road_trip", headers: @headers, params: JSON.generate(@parameters)
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    roadtrip = response_body[:data]
+
+    expect(roadtrip).to be_a(Hash)
+    expect(roadtrip.keys).to include(:id, :type, :attributes)
+    expect(roadtrip[:id]).to eq(nil)
+    expect(roadtrip[:type]).to eq("roadtrip")
+    expect(roadtrip[:attributes]).to be_a(Hash)
+
+    expect(roadtrip[:attributes].keys).to include(:start_city, :end_city, :travel_time, :weather_at_eta)
+    expect(roadtrip[:attributes][:start_city]).to be_a(String)
+    expect(roadtrip[:attributes][:end_city]).to be_a(String)
+    expect(roadtrip[:attributes][:travel_time]).to be_a(String)
+    expect(roadtrip[:attributes][:weather_at_eta]).to be_a(Hash)
+
+    expect(roadtrip[:attributes][:weather_at_eta].keys).to include(:temperature, :conditions)
+
+    expect(roadtrip[:attributes][:weather_at_eta][:temperature]).to be_an(Integer).or be_a(Float)
+    expect(roadtrip[:attributes][:weather_at_eta][:conditions]).to be_a(String)
+  end
 end
