837e85d36653e461d3bdca4c8aae178b55b434eb
Sarah Hall <swatson1994@yahoo.com>
2022-08-09 14:51:28 -0400

    Remove api key from required params, use conditional to return error

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::RoadTripController < ApplicationController
 
   def create
-    trip = GeoFacade.create_directions(trip_params[:origin], trip_params[:destination])
-    forecast = OpenWeatherFacade.create_future_weather(trip.lat, trip.lon, trip.hours)
-    render json: RoadTripSerializer.format_road_trip(trip, forecast)
+    if !trip_params[:api_key] || !User.valid_key?(trip_params[:api_key])
+      json_response({message: "Unauthorized Request"}, 401)
+    else
+      trip = GeoFacade.create_directions(trip_params[:origin], trip_params[:destination])
+      forecast = OpenWeatherFacade.create_future_weather(trip.lat, trip.lon, trip.hours)
+      render json: RoadTripSerializer.format_road_trip(trip, forecast)
+    end
   end
 
   private
@@ -11,7 +15,6 @@ class Api::V1::RoadTripController < ApplicationController
   def trip_params
     params.require(:origin)
     params.require(:destination)
-    params.require(:api_key)
     params.permit(:origin, :destination, :api_key)
     end
 end
