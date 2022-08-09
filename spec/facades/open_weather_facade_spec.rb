8c091382c147f74a3385cb8b39d8a06ee1c8894b
Sarah Hall <swatson1994@yahoo.com>
2022-08-09 13:51:22 -0400

    Feat: add create future weather facade method, and fix test to take int

--- spec/facades/open_weather_facade_spec.rb
+++ spec/facades/open_weather_facade_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe OpenWeatherFacade, :vcr do
   end
 
   it 'will make an hourly poro for the furture, given a number of hours below 48' do
-    test = OpenWeatherFacade.create_future_weather('43.047945', '-76.147448', "10")
+    test = OpenWeatherFacade.create_future_weather('43.047945', '-76.147448', 10)
     expect(test).to be_an_instance_of(Hourly)
   end
 
   it 'will make a daily poro given a number of hours greater than or equal to 48' do
-    test = OpenWeatherFacade.create_future_weather('43.047945', '-76.147448', "56")
+    test = OpenWeatherFacade.create_future_weather('43.047945', '-76.147448', 56)
     expect(test).to be_an_instance_of(Daily)
   end
 end
